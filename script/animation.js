351ce901912701e51fb7f35218fcb271eaa03e6d
alanzeng17 <alanzeng17@gmail.com>
2017-06-20 19:17:47 -0500

    second commit

--- script/animation.js
+++ script/animation.js
@@ -1,13 +1,33 @@
-var h = "h";
+var time = null;
+var numPlayers = null;
+function getSelectedText(elementId) {
+    var elt = document.getElementById(elementId);
+
+    if (elt.selectedIndex == -1)
+        return null;
+
+    return elt.options[elt.selectedIndex].text;
+}
 var main = function() {
 
   $(".new-game-button").click(function() {
-
-    $("#button").fadeOut("slow", function () {
+      time = getSelectedText('time');
+      numPlayers = getSelectedText('players');
+      var fields = "<form>";
+      var count = 1;
+      while(count <= numPlayers) {
+        fields = fields.concat("Player "+ count + " name:<input type='text' class = 'textfield'>");
+        count++;
+      }
+      fields = fields.concat("</form><input type='submit' id='mySubmit' value='Submit'>");
+      $("#mySubmit").click(function()) {
+        var playerArr = document.getElementsByClassName('class_name');
+      }
+    /*$("#button").fadeOut("slow", function () {
       var div =$("<div id = 'button' class='wrapper'>hello</div>").hide();
       $(this).replaceWith(div);
       $("#button").fadeIn("slow");
-    });
+    });*/
 
   });
 
