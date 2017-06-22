5afb5338d80093d336d283ca92202e7ac81ec321
alanzeng17 <alanzeng17@gmail.com>
2017-06-21 20:52:26 -0500

    Rearraged js files, completed front end setup

--- script/animation.js
+++ script/animation.js
@@ -8,8 +8,9 @@
 var time = null;
 var newTime = null;
 var numPlayers = null;
+var playerArr = [];
 
-
+//Helper methods
 /*
 * Method to return the text of a given element id
 */
@@ -33,43 +34,45 @@ function timeConversion() {
   }
 }
 /*
+* Creates an array of the names of all players
+*/
+function createPlayerArray() {
+  var temp = document.getElementsByClassName('textfield');
+  for(var i = 0; i < numPlayers; i++) {
+    playerArr.push(temp[i].value);
+  }
+
+}
+function isPlayerArrayValid() {
+  for(var i = 0; i < numPlayers; i++) {
+    if(playerArr[i].value ==='') {
+      return false;
+    }
+  }
+  return true;
+}
+
+/*
 * Main start screen runner
 */
 var main = function() {
+
+  //1st page, Game mode selection
   //When new game button is clicked, run this function
   $("#ngButton").click(function() {
-      time = getSelectedText('time');
-      numPlayers = getSelectedText('players');
 
-      if(time[0] === '-' || numPlayers[0] === '-') {
-        alert("Please select the time and number of players.");
-      }
-      else {
-        timeConversion();
+      var sButtons = "<div id= secondButtons class = wrapper><input class='myButton' type='button' id='onlineButton' value='Play Online'><input class='myButton' type='button' id='papButton' value='Pass and Play'></div>";
 
-        //creates the entry fields based on given data
-        var fields = "<div id = 'nameEntry' class='wrapper'><form id = 'nameForm'>";
-        var count = 1;
-        while(count <= numPlayers) {
-          fields = fields.concat("Player "+ count + " name:<input type='text' class = 'textfield'><br><br>");
-          count++;
-        }
-        fields = fields.concat("</form><br><input class='myButton' type='button' id='onlineButton' value='Play Online'><input class='myButton' type='button' id='papButton' value='Pass and Play'></div>");
-        $("#mySubmit").click(function() {
-          var playerArr = document.getElementsByClassName('textfield');
-        });
+      //animation to go to second part(name entry/game select)
+      $("#button").fadeOut("slow", function () {
+      var div =$(sButtons).hide();
+      $("#button").replaceWith(div);
+      $("#secondButtons").fadeIn(550);
 
-        //animation to go to second part(name entry/game select)
-        $("#params").fadeOut("slow");
-        $("#button").fadeOut("slow", function () {
-        var div =$(fields).hide();
-        $(this).replaceWith(div);
-        $("#nameEntry").fadeIn(550);
-
-        //!add a back button animation!
+      //!add a back button animation!
       });
+      $("#helpDiv").replaceWith("<div id='helpDiv' class='wrapper'><p id= 'helpText'>Pass and Play: Play with your friends using one device.<br>Play Online: Join an existing lobby</p></div>");
 
-    }
   });
   $("#jgButton").click(function() {
     alert("Sorry, online play is still in development. Please visit https://github.com/alanzeng17/Avalon for updates on progress.");
@@ -81,6 +84,76 @@ var main = function() {
 
 
   //!add a back button click function!
+
+  //Second page, Game mode selection pt.2
+  $(document).on('click', '#papButton', function() {
+    var params = "<div id = 'params' class=container>" +
+"<label for='select'>Number of Players:</label><select id = 'players' value = 'select'><option value = 'x'> --Select Number-- </option><option value = '5'> 5 </option><option value = '6'> 6 </option><option value = '7'> 7 </option><option value = '8'> 8 </option><option value = '9'> 9 </option><option value = '10'> 10 </option></select>" +
+"<br><br><label for = 'time'> Set the time limit for each round: </label><select id = 'time' value = 'time'><option value = 'xx'> --Select Time-- </option><option value = 'NL'> No Limit </option><option value = '60'> 1:00 </option><option value = '90'> 1:30 </option><option value = '120'> 2:00 </option><option value = '150'> 2:30 </option><option value = '180'> 3:00 </option><option value = '210'> 3:30 </option><option value = '240'> 4:00 </option><option value = '270'> 4:30 </option>  <option value = '300'> 5:00 </option><option value = '330'> 5:30 </option><option value = '360'> 6:00 </option><option value = '390'> 6:30 </option></select><br><br><button id='nextButton' class = myButton>Next</button></div>";
+
+    $("#secondButtons").fadeOut("slow", function () {
+    var div =$(params).hide();
+    $(this).replaceWith(div);
+    $("#params").fadeIn(550);
+    });
+
+  });
+  $(document).on('click', '#onlineButton', function() {
+    alert("Sorry, online play is still in development. Please visit https://github.com/alanzeng17/Avalon for updates on progress.");
+    //#1. Trasition to a screen w/ 2 buttons, Create Lobby, Join Lobby
+  });
+
+
+  //Third Page, settings selection
+  $(document).on('click', "#nextButton", function() {
+    //Scan vals
+    time = getSelectedText("time");
+    numPlayers = getSelectedText("players");
+
+    //Check validity
+    if(time[0] === '-' || players[0] === '-') {
+      alert("Please fill in the Number of Players and Time Limit.");
+    }
+    else {
+      timeConversion();
+
+      //Help change
+      $("#helpDiv").replaceWith("<div id='helpDiv' class='wrapper'><p id= 'helpText'>Enter the players' names in order of position.</p></div>");
+
+      //Textfield transition
+      var fields = "<div id = 'nameEntry' class='wrapper'><form id = 'nameForm'>";
+      var count = 1;
+      while(count <= numPlayers) {
+        fields = fields.concat("Player "+ count + " name:<input type='text' class = 'textfield'><br><br>");
+        count++;
+      }
+      fields = fields.concat("</form><button id = 'sgButton' class = 'myButton'>Start Game</button></div>");
+
+      //animation to go to second part(name entry/game select)
+      $("#params").fadeOut("slow", function () {
+      var div =$(fields);
+      $(this).replaceWith(div);
+      $("#nameEntry").fadeIn(550);
+      });
+
+
+
+    }
+
+  });
+  //End of name entry, start pass and play game
+  $(document).on('click', '#sgButton',function() {
+    createPlayerArray();
+    var valid = isPlayerArrayValid();
+    if(!valid) {
+      alert("Please fill out fields for all players.");
+    }
+    else {
+      //BEGIN GAME
+    console.log("GAME STARTO");
+    console.log(playerArr.toString());
+    }
+  });
 
 
 
--- script/game.js
+++ script/game.js
@@ -1,14 +1,16 @@
+
+
 var main = function() {
-  $(document).on('click', '#papButton', function() {
 
 
 
-  });
-  $(document).on('click', '#onlineButton', function() {
-    alert("Sorry, online play is still in development. Please visit https://github.com/alanzeng17/Avalon for updates on progress.");
-    //#1. Trasition to a screen w/ 2 buttons, Create Lobby, Join Lobby
-  });
+
+
 
 }
 
 $(document).ready(main);
+
+/*
+
+*/
